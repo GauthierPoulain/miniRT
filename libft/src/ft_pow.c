dd68e16ae977f612e844530e2873b8d947406d52
gauthier poulain <gapoulai@student.42lyon.fr>
2021-02-22 15:31:47 +0100

    improve pow

--- libft/src/ft_pow.c
+++ libft/src/ft_pow.c
@@ -6,7 +6,7 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 12:22:23 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/02/08 12:33:33 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2021/02/22 15:31:17 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ long long	ft_pow(long long nb, long long power)
 {
 	if (power < 0)
 		return (0);
-	if (power < 1)
+	if (power == 0)
 		return (1);
-	else
-		return (nb * ft_pow(nb, power - 1));
+	return (nb * ft_pow(nb, power - 1));
 }
--- main.c
+++
@@ -1,7 +0,0 @@
-#include "./libft/libft.h"
-
-int main(int argc, char const *argv[])
-{
-	printf("%f", ft_atof("-1.4"));
-	return 0;
-}
