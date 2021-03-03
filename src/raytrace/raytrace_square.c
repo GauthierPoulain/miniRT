f5deec62e2b6218f12c6cae8b4bc7ff3b31773e3
gauthier poulain <gapoulai@student.42lyon.fr>
2021-03-03 12:51:07 +0100

    fix sqaures en fait

--- src/raytrace/raytrace_square.c
+++ src/raytrace/raytrace_square.c
@@ -6,50 +6,34 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 12:11:58 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/03/02 14:38:09 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2021/03/03 12:50:38 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-double	get_proj(t_vector origin, t_vector v1, t_vector v2)
+double	get_proj(t_vector origin, t_vector dir, t_vector pos)
 {
-	t_vector	proj;
-	t_vector	a;
-	t_vector	b;
+	t_vector	relative_pos;
 
-	a = vectorminus(v1, origin);
-	b = vectorminus(v2, origin);
-	proj = vectormutliply(a, dot(b, a) / pow(magnitude(a), 2));
-	// printf("a = %.20f\n", magnitude(a));
-	return (fabs(distance(get_vector(0, 0, 0), proj)));
+	relative_pos = vectorminus(pos, origin);
+	return (fabs(dot(dir, relative_pos)));
 }
 
 bool	check_edges(t_square sq, t_hit hit)
 {
-	t_vector	r0;
-	t_vector	p;
+	t_vector	up;
+	t_vector	right;
 
-	r0 = sq.normal;
-	r0 = vectorcross(get_vector(0, 1, 0), r0);
-	// printf("x = %.20f, y = %.20f, z = %.20f\n", r0.x, r0.y, r0.z);
-	p = vectoradd(sq.origin, vectormutliply(r0, sq.size));
-	if (get_proj(sq.origin, p, hit.pos) > sq.size)
+	if (sq.normal.x == 1 && sq.normal.y == 0 && sq.normal.z == 0)
+		up = get_vector(0, 1, 0);
+	else
+		up = get_normalize(vectorcross(get_vector(1, 0, 0), sq.normal));
+	if (get_proj(sq.origin, up, hit.pos) > sq.size)
 		return (false);
-	// r0 = vectorcross(r0, apply_rot(sq.normal, r0, get_vector(0, 1, 0)));
-	r0 = vectorcross(r0, get_vector(0, 1, 0));
-	p = vectoradd(sq.origin, vectormutliply(r0, sq.size));
-	if (get_proj(sq.origin, p, hit.pos) > sq.size)
+	right = get_normalize(vectorcross(up, sq.normal));
+	if (get_proj(sq.origin, right, hit.pos) > sq.size)
 		return (false);
-	// r0 = vectorcross(get_vector(1, 0, 0), r0);
-	// p = vectoradd(sq.origin, vectormutliply(r0, sq.size));
-	// if (get_proj(sq.origin, p, hit.pos) > sq.size)
-	// 	return (false);
-	// r0 = vectorcross(get_vector(1, 0, 0), r0);
-	// p = vectoradd(sq.origin, vectormutliply(r0, sq.size));
-	// if (get_proj(sq.origin, p, hit.pos) > sq.size)
-	// 	return (false);
-	// printf("salut = %.20f\n", get_proj(sq.origin, p, hit.pos));
 	return (true);
 }
 
